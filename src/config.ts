6a8937ee2db6f2fdf179a856e3e80190396c01e7
江近亦 <jingjinyi789@gmail.com>
2025-11-28 18:50:39 +0800

    修复友链接力开往链接

--- src/config.ts
+++ src/config.ts
@@ -240,6 +240,7 @@ export const navBarConfig: NavBarConfig = {
 			name: "开往",
 			url: "https://www.travellings.cn/go.html",
 			icon: "fa6-solid:train-subway",
+			external:true,
 		},
 		// 支持自定义导航栏链接,并且支持多级菜单,3.1版本新加
 		// {
